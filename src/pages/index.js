69cd94bcdf895ea9977a836064772df05be21d65
Mger <mherphoto@gmail.com>
2021-02-08 13:14:56 +0300

    8 sprint-review 1

--- src/pages/index.js
+++ src/pages/index.js
@@ -18,19 +18,15 @@ import UserInfo from '../components/UserInfo.js';
 import FormValidator from '../components/FormValidator.js';
 import Section from '../components/Section.js';
 
-// валидация для попапа редактирования профиля
 const profileFormValidator = new FormValidator(validationConfig, formProfileElement);
 profileFormValidator.enableValidation();
 
-// валидация для попапа добавления карточки
 const newCardFormValidator = new FormValidator(validationConfig, formElementCards);
 newCardFormValidator.enableValidation();
 
-// создание экземпляра класса для попапа изображения
 const popupWithImage = new PopupWithImage('.popup_img');
 popupWithImage.setEventListeners();
 
-// отрисовка карточек в секции cards
 const cardList = new Section({
     items: initialCards,
     renderer: (item) => {
@@ -40,7 +36,6 @@ const cardList = new Section({
 }, '.cards');
 cardList.renderItems();
 
-// создание экземпляра класса для попапа добавления новой карточки
 const popupCardsWithForm = new PopupWithForm(
     '.popup_add-new-card',
     {
@@ -51,10 +46,8 @@ const popupCardsWithForm = new PopupWithForm(
     });
 popupCardsWithForm.setEventListeners();
 
-// создание экземпляра класса для отображения информации о пользователе
 const userInfo = new UserInfo({ name: '.profile__name', job: '.profile__job' });
 
-// создание экземпляра класса для попапа редактирования профиля
 const popupProfileWithForm = new PopupWithForm(
     '.popup_edit-profile',
     {
@@ -65,20 +58,17 @@ const popupProfileWithForm = new PopupWithForm(
 );
 popupProfileWithForm.setEventListeners();
 
-// функция для создания экземпляра карточки
 function createCard(item) {
     const card = new Card(item, '.card', popupWithImage);
     const cardElements = card.generateCard();
     return cardElements;
 }
 
-// нажатие на кнопку добавления новой карточки
 profileAddButton.addEventListener('click', function () {
     popupCardsWithForm.open();
     newCardFormValidator.resetValidation();
 });
 
-// нажатие на кнопку редактирования профиля
 profileEditButton.addEventListener('click', function () {
     popupProfileWithForm.open();
     nameInput.value = userInfo.getUserInfo().name;
